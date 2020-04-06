efddfc26ff722e4b856b666602e0a357c9c7740f
robertomurion <roberto.murion@hotmail.com>
2020-04-06 00:05:42 -0300

    initial commit

---
+++ src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,10 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * Nome do caminhado do JNDI datasource Tomcat
+ * @author renato
+ *
+ */
+public class VariavelConexaoUtil {
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+}
